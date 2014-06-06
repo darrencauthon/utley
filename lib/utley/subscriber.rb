abaa8ed67bd0ee49d9fe2bde4f70003f0cd31b2a
Darren Cauthon <darren@cauthon.com>
2014-06-06 10:13:14 -0500

    Allow multiple subscribers to be returned.

--- lib/utley/subscriber.rb
+++ lib/utley/subscriber.rb
@@ -3,9 +3,9 @@ module Utley
   module Subscriber
 
     def self.for publisher, event
-      registry_record = registry.select { |x| x[:publisher_id] == publisher.id &&
-                                              x[:event_id]     == event.id }.first
-      [build_from(registry_record)]
+      registry_records = registry.select { |x| x[:publisher_id] == publisher.id &&
+                                              x[:event_id]     == event.id }
+      registry_records.map { |x| build_from x }
     end
 
   end
--- spec/utley/subscriber_spec.rb
+++ spec/utley/subscriber_spec.rb
@@ -11,7 +11,8 @@ describe Utley::Subscriber do
         ['b', '2', 1],
         ['b', '2', 3],
       ].map { |x| Struct.new(:publisher_id, :event_id, :registry_index).new(*x) }.each do |example|
-        describe "multiple examples" do
+
+        describe "multiple examples of one match" do
 
           let(:publisher) { Struct.new(:id).new(example.publisher_id) }
           let(:event)     { Struct.new(:id).new(example.event_id) }
@@ -38,6 +39,46 @@ describe Utley::Subscriber do
             results = Utley::Subscriber.for publisher, event
 
             results.first.must_be_same_as subscriber
+          end
+
+        end
+
+        describe "multiple examples of two matches" do
+
+          let(:publisher) { Struct.new(:id).new(example.publisher_id) }
+          let(:event)     { Struct.new(:id).new(example.event_id) }
+
+          let(:our_registry_item_1) do
+            { publisher_id: example.publisher_id, event_id: example.event_id, something_specific: 1 }
+          end
+
+          let(:our_registry_item_2) do
+            { publisher_id: example.publisher_id, event_id: example.event_id, something_different: 2 }
+          end
+
+          let(:registry) do
+            [ 
+              { publisher_id: example.publisher_id + 'a', event_id: example.event_id + 'b' },
+              { publisher_id: example.publisher_id + 'b', event_id: example.event_id + 'c' },
+              { publisher_id: example.publisher_id + 'c', event_id: example.event_id + 'd' },
+              { publisher_id: example.publisher_id + 'd', event_id: example.event_id + 'e' }
+            ].insert(example.registry_index, our_registry_item_2)
+             .insert(example.registry_index, our_registry_item_1)
+          end
+
+          it "should return both subscribers" do
+            subscriber_1 = Object.new
+            subscriber_2 = Object.new
+
+            Utley::Subscriber.stubs(:registry).returns registry
+            Utley::Subscriber.stubs(:build_from).with(our_registry_item_1).returns subscriber_1
+            Utley::Subscriber.stubs(:build_from).with(our_registry_item_2).returns subscriber_2
+
+            results = Utley::Subscriber.for publisher, event
+
+            results.count.must_equal 2
+            results[0].must_be_same_as subscriber_1
+            results[1].must_be_same_as subscriber_2
           end
 
         end
