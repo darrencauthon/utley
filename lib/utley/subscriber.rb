e8bd1bd1eae19067c1060d6345444002d3397983
Darren Cauthon <darren@cauthon.com>
2014-06-06 09:57:11 -0500

    Build a subscriber by looking up information in the subscriber registry.

--- lib/utley/subscriber.rb
+++ lib/utley/subscriber.rb
@@ -3,7 +3,9 @@ module Utley
   module Subscriber
 
     def self.for publisher, event
-      build_from(registry.first)
+      registry_record = registry.select { |x| x[:publisher_id] == publisher.id &&
+                                              x[:event_id]     == event.id }.first
+      build_from registry_record
     end
 
   end
--- spec/utley/subscriber_spec.rb
+++ spec/utley/subscriber_spec.rb
@@ -6,19 +6,42 @@ describe Utley::Subscriber do
 
     describe "given a publisher and an event" do
 
-      let(:publisher) { Struct.new(:id).new('a') }
-      let(:event)     { Struct.new(:id).new('1') }
+      [
+        ['a', '1', 0],
+        ['b', '2', 1],
+        ['b', '2', 3],
+      ].map { |x| Struct.new(:publisher_id, :event_id, :registry_index).new(*x) }.each do |example|
+        describe "multiple examples" do
 
-      it "should look up who is subscribed to this publisher and this event" do
-        subscriber = Object.new
-        registry   = [ { publisher_id: 'a', event_id: '1' } ]
+          let(:publisher) { Struct.new(:id).new(example.publisher_id) }
+          let(:event)     { Struct.new(:id).new(example.event_id) }
 
-        Utley::Subscriber.stubs(:registry).returns registry
-        Utley::Subscriber.stubs(:build_from).with(registry.first).returns subscriber
+          let(:our_registry_item) do
+            { publisher_id: example.publisher_id, event_id: example.event_id }
+          end
 
-        result = Utley::Subscriber.for publisher, event
+          let(:registry) do
+            [ 
+              { publisher_id: example.publisher_id + 'a', event_id: example.event_id + 'b' },
+              { publisher_id: example.publisher_id + 'b', event_id: example.event_id + 'c' },
+              { publisher_id: example.publisher_id + 'c', event_id: example.event_id + 'd' },
+              { publisher_id: example.publisher_id + 'd', event_id: example.event_id + 'e' }
+            ].insert(example.registry_index, our_registry_item)
+          end
 
-        result.must_be_same_as subscriber
+          it "should look up who is subscribed to this publisher and this event" do
+            subscriber = Object.new
+
+            Utley::Subscriber.stubs(:registry).returns registry
+            Utley::Subscriber.stubs(:build_from).with(our_registry_item).returns subscriber
+
+            result = Utley::Subscriber.for publisher, event
+
+            result.must_be_same_as subscriber
+          end
+
+        end
+
       end
 
     end
