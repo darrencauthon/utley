a9346eb96d0e481246c4f671f96e24eafa1b3f8a
Darren Cauthon <darren@cauthon.com>
2014-06-06 10:16:49 -0500

    More refactoring.

--- lib/utley/subscriber.rb
+++ lib/utley/subscriber.rb
@@ -3,11 +3,15 @@ module Utley
   module Subscriber
 
     def self.for publisher, event
-      registry_entries_for(publisher, event).map { |x| build_from x }
+      build_subscribers_from registry_entries_for(publisher, event)
     end
 
     class << self
       private
+
+      def build_subscribers_from registry_entries
+        registry_entries.map { |x| build_from x }
+      end
 
       def registry_entries_for publisher, event
         registry.select { |x| x[:publisher_id] == publisher.id &&
