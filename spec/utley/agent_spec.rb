d9439840d12af44f146a4cb9b6afe3caf71a9126
Darren Cauthon <darren@cauthon.com>
2014-06-09 23:42:14 -0500

    No need for this on the base class yet.

--- spec/utley/agent_spec.rb
+++ spec/utley/agent_spec.rb
@@ -1,12 +1,4 @@
 require_relative '../spec_helper'
 
 describe Utley::Agent do
-
-  describe "receive" do
-    it "should exist" do
-      agent = Utley::Agent.new
-      agent.receive nil
-    end
-  end
-
 end
