da05ada8165f5515635f26384ab03fd756174718
Darren Cauthon <darren@cauthon.com>
2014-06-09 23:38:46 -0500

    Stop doing this Darren.

--- spec/utley/agent_spec.rb
+++ spec/utley/agent_spec.rb
@@ -2,10 +2,6 @@ require_relative '../spec_helper'
 
 describe Utley::Agent do
 
-  it "should exist" do
-    true.must_equal true  
-  end
-
   describe "receive" do
     it "should exist" do
       agent = Utley::Agent.new
